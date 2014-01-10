305a3d4000db161e1f2fe039f22fab365786f223
Wilfred <sangnh@joomsolutions.com>
2014-01-10 13:44:03 +0700

    Style Navigation

--- lib/nav.php
+++ lib/nav.php
@@ -10,46 +10,35 @@
  *   <li class="menu-home"><a href="/">Home</a></li>
  *   <li class="menu-sample-page"><a href="/sample-page/">Sample Page</a></li>
  */
-// class dw_timeline_Nav_Walker extends Walker_Nav_Menu {
-//   function check_current($classes) {
-//     return preg_match('/(current[-_])|active|dropdown/', $classes);
-//   }
+class dw_timeline_Nav_Walker extends Walker_Nav_Menu {
+  function check_current($classes) {
+    return preg_match('/(current[-_])|active|dropdown/', $classes);
+  }
 
-//   function start_lvl(&$output, $depth = 0, $args = array()) {
-//     $output .= "\n<ul class=\"dropdown-menu\">\n";
-//   }
+  function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
+    $item_html = '';
+    parent::start_el($item_html, $item, $depth, $args);
 
-//   function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
-//     $item_html = '';
-//     parent::start_el($item_html, $item, $depth, $args);
+    if ( $item->is_dropdown && $depth === 0 ) {
+      $item_html = str_replace('</a>', '<span class="glyphicon glyphicon-chevron-down"></span></a>', $item_html);
+    } elseif ( $item->is_dropdown && $depth > 0 ) {
+      $item_html = str_replace('</a>', '<span class="glyphicon glyphicon-chevron-right"></span></a>', $item_html);
+    }
 
-//     if ( ! $item->is_dropdown && $depth === 0 ) {
-//       $item_html = str_replace('<a', '<a data-hover="'.$item->title.'" ', $item_html);
-//     } elseif ($item->is_dropdown && ($depth === 0)) {
-//       $item_html = str_replace('<a', '<a class="dropdown-toggle" data-toggle="dropdown" data-hover="'.$item->title.'" data-target="#"', $item_html);
-//       $item_html = str_replace('</a>', ' <b class="caret"></b></a>', $item_html);
-//     }
-//     elseif (stristr($item_html, 'li class="divider')) {
-//       $item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);
-//     }
-//     elseif (stristr($item_html, 'li class="dropdown-header')) {
-//       $item_html = preg_replace('/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html);
-//     }
+    $item_html = apply_filters('dw_timeline_wp_nav_menu_item', $item_html);
+    $output .= $item_html;
+  }
 
-//     $item_html = apply_filters('dw_timeline_wp_nav_menu_item', $item_html);
-//     $output .= $item_html;
-//   }
+  function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
+    $element->is_dropdown = ((!empty($children_elements[$element->ID]) && (($depth + 1) < $max_depth || ($max_depth === 0))));
 
-//   function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) {
-//     $element->is_dropdown = ((!empty($children_elements[$element->ID]) && (($depth + 1) < $max_depth || ($max_depth === 0))));
+    if ($element->is_dropdown) {
+      $element->classes[] = 'has-submenu';
+    }
 
-//     if ($element->is_dropdown) {
-//       $element->classes[] = 'dropdown';
-//     }
-
-//     parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
-//   }
-// }
+    parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
+  }
+}
 
 /**
  * Remove the id="" on nav menu items
@@ -75,21 +64,17 @@ add_filter('nav_menu_item_id', '__return_null');
  * Remove the container
  * Use dw_timeline_Nav_Walker() by default
  */
-// function dw_timeline_nav_menu_args($args = '') {
-//   $dw_timeline_nav_menu_args['container'] = false;
+function dw_timeline_nav_menu_args($args = '') {
+  $dw_timeline_nav_menu_args['container'] = false;
 
-//   if (!$args['items_wrap']) {
-//     $dw_timeline_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
-//   }
+  if (!$args['items_wrap']) {
+    $dw_timeline_nav_menu_args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
+  }
 
-//   if (current_theme_supports('bootstrap-top-navbar') && !$args['depth']) {
-//     $dw_timeline_nav_menu_args['depth'] = 2;
-//   }
+  if (!$args['walker']) {
+    $dw_timeline_nav_menu_args['walker'] = new dw_timeline_Nav_Walker();
+  }
 
-//   if (!$args['walker']) {
-//     $dw_timeline_nav_menu_args['walker'] = new dw_timeline_Nav_Walker();
-//   }
-
-//   return array_merge($args, $dw_timeline_nav_menu_args);
-// }
-// add_filter('wp_nav_menu_args', 'dw_timeline_nav_menu_args');
+  return array_merge($args, $dw_timeline_nav_menu_args);
+}
+add_filter('wp_nav_menu_args', 'dw_timeline_nav_menu_args');
