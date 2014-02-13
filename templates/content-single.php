84431adcef881930e0bd7f8de67d5e94e104fd5e
Wilfred <sangnh@joomsolutions.com>
2014-02-13 09:21:43 +0700

    Fix bug as review of wordpress.org
    - This theme doesn't seem to display tags.

--- templates/content-single.php
+++ templates/content-single.php
@@ -6,9 +6,20 @@
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
+      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'dw-timeline'), 'after' => '</p></nav>')); ?>
     </div>
     <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'dw-timeline'), 'after' => '</p></nav>')); ?>
+      <?php
+        $tags_list = get_the_tag_list( '', __( ', ', 'dw-timeline' ) );
+        if ( $tags_list ) :
+      ?>
+        <div class="entry-tags">
+          <span class="tags-title"><?php _e('Tags: ','dw-timeline') ?></span>
+          <span class="tags-links">
+          <?php printf( __( '%1$s', 'dw-timeline' ), $tags_list ); ?>
+        </span>
+        </div>
+      <?php endif; ?>
     </footer>
   </article>
   <?php comments_template('/templates/comments.php'); ?>
