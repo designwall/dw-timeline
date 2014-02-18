9949060726f31373f5f9ee9df0cf83566c355740
Wilfred <sangnh@joomsolutions.com>
2014-02-18 16:32:42 +0700

    Update script

--- lib/scripts.php
+++ lib/scripts.php
@@ -5,6 +5,8 @@
 function dw_timeline_scripts() {
   wp_enqueue_style('dw_timeline_main', get_template_directory_uri() . '/assets/css/main.css', false, '6c39f42987ae297a5a21e2bb35bf3402');
 
+  wp_enqueue_style( 'dw_timeline_style', get_stylesheet_uri(), false, 'c1a58eb4baaf24c3771085df3d54ff8d' );
+
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
